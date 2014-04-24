6425f412b9a7be1e44828b121ffccc0e977ac5f3
MMManageD <edscclo1@lamp102.cloudaccess.net>
2014-04-23 23:55:53 -0400

    added wp-config to gitignore

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'edsc');
+define('DB_NAME', 'edscclo1');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'edscclo1');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'drag0nball');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
